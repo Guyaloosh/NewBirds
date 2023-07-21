3eec1b7fff9a8fa2f7f8dcdb8064fb380d3c3a72
Guyaloosh <guyaloosh@gmail.com>
2023-07-21 17:14:18 +0300

    now add a read me note

--- ProjectBirdsz/Program.cs
+++ ProjectBirdsz/Program.cs
@@ -13,7 +13,7 @@ namespace ProjectBirdsz
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AddCageForm());
+            Application.Run(new StartForm());
         }
     }
 }
